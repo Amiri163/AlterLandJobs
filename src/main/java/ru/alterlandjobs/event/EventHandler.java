d8999117c67c48e49e0af6846668dfb6e7c0d072
Amiri <iuak228@gmail.com>
2023-11-20 18:09:22 +0300

    pre final

--- src/main/java/ru/alterlandjobs/event/EventHandler.java
+++ src/main/java/ru/alterlandjobs/event/EventHandler.java
@@ -5,6 +5,8 @@ import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.client.particle.ParticleManager;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.Tessellator;
@@ -12,10 +14,12 @@ import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.client.renderer.vertex.VertexFormat;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.particles.ParticleTypes;
 import net.minecraft.util.Util;
 import net.minecraft.util.math.vector.Matrix4f;
 import net.minecraft.util.math.vector.Vector3f;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
 import net.minecraftforge.event.TickEvent;
@@ -28,29 +32,42 @@ import org.lwjgl.opengl.GL11;
 public class EventHandler {
     static boolean flag = true;
 
+    static double startXF = 212; // Мировые координаты первой точки
+    static double startYF = 70;
+    static double startZF = 222;
+
     @SubscribeEvent
     public static void onRenderWorldLast(RenderWorldLastEvent event) {
-     if(flag)
-        renderLight(event.getMatrixStack(), event.getPartialTicks());
+        if (flag)
+            renderLight(event.getMatrixStack(), event.getPartialTicks());
     }
 
     @SubscribeEvent
     public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
         if (event.phase == TickEvent.Phase.END && event.player instanceof ServerPlayerEntity) {
             ServerPlayerEntity player = (ServerPlayerEntity) event.player;
-            int posX = 212;
-            int posY = 70 - 1; // Проверяем блок под игроком
-            int posZ = 222;
 
-            if (player.getX() == 212 && player.getY() == posY && player.getZ() == 222) {
-                player.sendMessage(new StringTextComponent("aboba"), Util.NIL_UUID);
+            if (Math.round(player.getX()) == startXF && Math.round(player.getY()) == startYF && Math.round(player.getZ()) == startZF) {
+
+                startXF = 200; // Новые координаты для луча по оси X
+                startYF = 60;  // Новые координаты для луча по оси Y
+                startZF = 200;
+
                 flag = false;
+
             }
         }
     }
 
     public static void renderLight(MatrixStack matrixStack, float particalTicks) {
+        double endY = 200; // Вторая координата по оси Y
+        double startX = startXF;
+        double startZ = startZF;
+
+        double startY = 10;
+
         Minecraft mc = Minecraft.getInstance();
+        ParticleManager particleManager = Minecraft.getInstance().particleEngine;
         Tessellator tessellator = Tessellator.getInstance();
         BufferBuilder bufferBuilder = tessellator.getBuilder();
         Matrix4f matrix4f = matrixStack.last().pose();
@@ -60,15 +77,8 @@ public class EventHandler {
         RenderSystem.enableBlend();
         RenderSystem.disableTexture();
         RenderSystem.defaultBlendFunc();
-        RenderSystem.disableDepthTest();
 
         bufferBuilder.begin(3, DefaultVertexFormats.POSITION_COLOR);
-
-        double startX = 212; // Мировые координаты первой точки
-        double startY = 40;
-        double startZ = 222;
-
-        double endY = 150; // Ваша вторая координата по оси Y
 
         PlayerEntity player = mc.player;
         double playerX = player.getX();
@@ -87,16 +97,15 @@ public class EventHandler {
 
         endY -= interpolatedPlayerY;
 
-
         bufferBuilder.vertex(matrix4f, (float) startX, (float) startY, (float) startZ).color((int) color.x(), (int) color.y(), (int) color.z(), 255).endVertex();
         bufferBuilder.vertex(matrix4f, (float) startX, (float) endY, (float) startZ).color((int) color.x(), (int) color.y(), (int) color.z(), 255).endVertex();
+        Particle particle = particleManager.createParticle(ParticleTypes.EXPLOSION, startXF, startYF, startZF, 10, 10, 10);
+        particle.scale(0.1f); // Устанавливает размер партикла
+        particleManager.add(particle);
 
         tessellator.end();
-
-        RenderSystem.enableDepthTest();
         RenderSystem.enableTexture();
         RenderSystem.disableBlend();
-
         matrixStack.popPose();
     }
 }
