f94cfea075be8d5ebdf4c8f6d5741512839bd2c9
Amiri <iuak228@gmail.com>
2023-11-20 16:34:28 +0300

    pre final

--- src/main/java/ru/alterlandjobs/event/EventHandler.java
+++ src/main/java/ru/alterlandjobs/event/EventHandler.java
@@ -3,6 +3,7 @@ package ru.alterlandjobs.event;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.block.Blocks;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.RenderHelper;
@@ -11,10 +12,13 @@ import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.client.renderer.vertex.VertexFormat;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.util.Util;
 import net.minecraft.util.math.vector.Matrix4f;
 import net.minecraft.util.math.vector.Vector3f;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
+import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.lwjgl.opengl.GL11;
@@ -22,17 +26,33 @@ import org.lwjgl.opengl.GL11;
 
 @Mod.EventBusSubscriber
 public class EventHandler {
-
+    static boolean flag = true;
 
     @SubscribeEvent
     public static void onRenderWorldLast(RenderWorldLastEvent event) {
+     if(flag)
+        renderLight(event.getMatrixStack(), event.getPartialTicks());
+    }
+
+    @SubscribeEvent
+    public static void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        if (event.phase == TickEvent.Phase.END && event.player instanceof ServerPlayerEntity) {
+            ServerPlayerEntity player = (ServerPlayerEntity) event.player;
+            int posX = 212;
+            int posY = 70 - 1; // Проверяем блок под игроком
+            int posZ = 222;
+
+            if (player.getX() == 212 && player.getY() == posY && player.getZ() == 222) {
+                player.sendMessage(new StringTextComponent("aboba"), Util.NIL_UUID);
+                flag = false;
+            }
+        }
+    }
+
+    public static void renderLight(MatrixStack matrixStack, float particalTicks) {
         Minecraft mc = Minecraft.getInstance();
-        if (mc.player == null) return;
-
-
         Tessellator tessellator = Tessellator.getInstance();
         BufferBuilder bufferBuilder = tessellator.getBuilder();
-        MatrixStack matrixStack = event.getMatrixStack();
         Matrix4f matrix4f = matrixStack.last().pose();
         Vector3f color = new Vector3f(255, 0, 0); // Цвет линии (R, G, B)
 
@@ -56,9 +76,9 @@ public class EventHandler {
         double playerZ = player.getZ();
 
         // Интерполяция между предыдущей и текущей позицией игрока
-        double interpolatedPlayerX = player.xo + (playerX - player.xo) * event.getPartialTicks();
-        double interpolatedPlayerY = player.yo + (playerY - player.yo) * event.getPartialTicks();
-        double interpolatedPlayerZ = player.zo + (playerZ - player.zo) * event.getPartialTicks();
+        double interpolatedPlayerX = player.xo + (playerX - player.xo) * particalTicks;
+        double interpolatedPlayerY = player.yo + (playerY - player.yo) * particalTicks;
+        double interpolatedPlayerZ = player.zo + (playerZ - player.zo) * particalTicks;
 
         // Корректировка координат относительно интерполированной позиции игрока
         startX -= interpolatedPlayerX + 0.5;
